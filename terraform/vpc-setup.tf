b3a552eaefe5be2a1776a3a2d1ecd9745839b9a5
lot8229 <lot8229@kakao.com>
2022-12-06 20:48:27 +0900

    terraform vpc_resources add

--- terraform/vpc-setup.tf
+++ terraform/vpc-setup.tf
@@ -10,11 +10,28 @@ locals {
   cluster_name = "msa_kube"
 }
 
-resource "aws_vpc" "this" {
+resource "aws_vpc" "msa_kube" {
   cidr_block = local.cidr
   tags = {Name = local.vpc_name}
 }
 
-resource "aws_default_route_table" "this" {
-  
+resource "aws_default_route_table" "default_rt" {
+  default_route_table_id = aws_vpc.msa_kube.default_route_table_id
+  tags = { Name = "${local.vpc_name}-default" }
+}
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.msa_kube.id
+  tags = { Name = "${local.vpc_name}-default" }
+}
+resource "aws_security_group" "security_group" {
+  vpc_id = aws_vpc.msa_kube.id
+  tags = { Name = "${local.vpc_name}-igw" }
+}
+resource "aws_internet_gateway" "name" {
+  vpc_id = aws_vpc.msa_kube.id
+  tags = { Name = "${local.vpc_name}-igw" }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.msa_kube.id
+  tags   = { Name = "${local.vpc_name}-public" }
 }
