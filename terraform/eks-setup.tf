3cbb153e7eeca835a3b0cba446df1c73eff3ec00
lot8229 <lot8229@kakao.com>
2022-12-22 16:47:29 +0900

    add ekf fargate profile

--- terraform/eks-setup.tf
+++ terraform/eks-setup.tf
@@ -20,7 +20,6 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceControlle
   policy_arn = "arm:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role = aws_iam_role.cluster.name
 }
-
 resource "aws_eks_cluster" "eks_cluster" {
   name = local.cluster_name
   role_arn = aws_iam_role.cluster.arn
@@ -33,3 +32,34 @@ resource "aws_eks_cluster" "eks_cluster" {
   ]
 }
 
+
+resource "aws_iam_role" "pod_execution" {
+  name = "${local.cluster_name}-eks-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Effect = "Allow",
+        Principal = {
+            Service = "eks-fargate-pods.amazonaws.com"
+        },
+        Action = "sts.AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_policy_attachment" "pod_execution_AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role = aws_iam_role.pod_execution.name
+}
+resource "aws_eks_fargate_profile" "default" {
+    cluster_name = aws_eks_cluster.eks_cluster.name
+    fargate_profile_name = "fp-default"
+    pod_execution_role_arn = aws_iam_role.pod_execution.arn
+    subnet_ids = aws_subnet.private[*].id
+    selector {
+        namespace = "default"
+    }
+    selector {
+        namespace = "kube-system"
+    }
+}
