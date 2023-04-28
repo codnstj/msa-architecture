3c8230b24c6ee5d73f28a14456dc6054fc0d03d5
lot8229 <lot8229@kakao.com>
2023-04-28 10:53:11 +0900

    construct User Model

--- django/user/models.py
+++ django/user/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 # Create your models here.
-class User:
-    userEmail = models.CharField(max_length=255)
-    userName = models.CharField(max_length=20)
-    profileImage = models.CharField(max_length=100)
-    joinDate = models.DateField()
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','email']
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email fieild must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        return self.create_user(email, password, **extra_fields)
